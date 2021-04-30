a60a9b58b69a4eacec0e6ed029b5a891b577c86e
tayutaedomo <tayutaedomo@gmail.com>
2021-05-01 00:27:58 +0900

    add: Function template for predict api

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 def hello_get(request):
     return 'Hello World!'
+
+
+def predict_category(request):
+    fields = {}
+    for key, value in request.form.to_dict().items():
+        fields[key] = value
+        print('Processed field: %s %s' % (key, value))
+
+    size = 0
+    files = request.files.to_dict()
+    for file_name, file in files.items():
+        data = file.read()
+        size = len(data)
+        print('Processed file size: %s' % size)
+        print(data)
+
+    return 'OK'
